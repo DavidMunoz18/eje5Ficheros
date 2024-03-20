3a1cc2f46fdca24e94442210e04e8231e77e2420
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-03-20 10:54:07 +0100

    Add files via upload

---
+++ ejercicio5Ficheros/Controladores/Program.cs
@@ -0,0 +1,41 @@
+﻿using ejercicio5Ficheros.Servicios;
+
+namespace ejercicio5Ficheros.Controladores
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            string rutaFichero = "C:\\Users\\csi23-dmunnco\\Desktop\\doc.txt";
+
+            MenuInterfaz mi = new MenuImplementacion();
+            OperativaInterfaz oi = new OperativaImplementacion();
+
+            bool cerrarMenu = false;
+
+            int opcion;
+
+            while(!cerrarMenu)
+            {
+                opcion = mi.mostrarMenuPrincipal();
+
+                switch(opcion)
+                {
+                    case 0:
+                        cerrarMenu = true;
+                        break;
+                    case 1:
+                        oi.lineaEspecifica(rutaFichero);
+                        break;
+                    case 2:
+                        oi.posicionEspecifica(rutaFichero);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ ejercicio5Ficheros/Servicios/MenuImplementacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5Ficheros.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public int mostrarMenuPrincipal()
+        {
+            int opcion;
+            Console.WriteLine("################################");
+            Console.WriteLine("0. Cerrar menu");
+            Console.WriteLine("1. Modificar una linea especifica");
+            Console.WriteLine("2. Modificar una posicion especifica");
+            Console.WriteLine("################################");
+            opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+    }
+}
